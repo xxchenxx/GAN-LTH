6ec17d3c3bf15499f29454780041b24fbf9c57a8
Ch4nYH <hfercxx@hotmail.com>
2021-10-07 15:06:52 +0800

    update

--- src/SNGAN/1007_impgd.sh
+++ src/SNGAN/1007_impgd.sh
@@ -1,9 +1,9 @@
 
 
 CUDA_VISIBLE_DEVICES=4 nohup python train_impgd.py --model sngan_cifar10 --exp_name sngan_cifar10 --init-path initial_weight_1 --load-path logs/sngan_cifar10_2021_08_24_01_28_02 --random_seed 1 --use-kd-D > 1007_impgdkd_seed1_GPU4.out &
-
+sleep 2s
 CUDA_VISIBLE_DEVICES=5 nohup python train_impgd.py --model sngan_cifar10 --exp_name sngan_cifar10 --init-path initial_weight_2 --load-path logs/sngan_cifar10_2021_08_24_01_28_00 --random_seed 2 --use-kd-D > 1007_impgdkd_seed2_GPU5.out &
-
+sleep 2s
 CUDA_VISIBLE_DEVICES=6 nohup python train_impgd.py --model sngan_cifar10 --exp_name sngan_cifar10 --init-path initial_weight_3 --load-path logs/sngan_cifar10_2021_08_24_04_58_12 --random_seed 3 --use-kd-D > 1007_impgdkd_seed3_GPU6.out &
-
+sleep 2s
 CUDA_VISIBLE_DEVICES=7 nohup python train_impgd.py --model sngan_cifar10 --exp_name sngan_cifar10 --init-path initial_weight_4 --load-path logs/sngan_cifar10_2021_08_24_01_28_05 --random_seed 4 --use-kd-D > 1007_impgdkd_seed4_GPU7.out &
